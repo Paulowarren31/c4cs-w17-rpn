0e73dd8307ece7b749acee7cf8975e1facd6d134
Paulo Warren <paulowar@umich.edu>
2017-03-10 17:40:33 -0500

    implemented calculator and added tests

--- rpn.py
+++ rpn.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python3
 
+
+def add(a,b):
+  return a + b
+
+def sub(a, b):
+  return b - a
+
+def mult(a, b):
+  return a * b
+
+def div(a, b):
+  return b / a
+
+operators = {
+    '+': add,
+    '-': sub,
+    '*': mult,
+    '/': div
+    }
+    
 def calculate(arg):
-    pass
+  stack = []
+  split = arg.split(' ')
+  for token in split:
+    if token.isdigit(): 
+      stack.append(int(token))
+    else:
+      if token in operators and len(stack) >= 2:
+        a = stack.pop()
+        b = stack.pop()
+        stack.append(operators[token](a, b))
+      else:
+        print('Error: Malformed expression')
+        return 
+
+  if len(stack) != 1:
+    print('Error: Malformed expression')
+    return
+  else:
+    print(float(stack[len(stack) - 1]))
+    return stack.pop()
+
 
 def main():
     while True:
--- test_rpn.py
+++ test_rpn.py
@@ -6,3 +6,28 @@ class TestBasics(unittest.TestCase):
   def test_add(self):
     result = rpn.calculate("1 1 +")
     self.assertEqual(2, result)
+    result = rpn.calculate("2 5 + 2 2 + +")
+    self.assertEqual(11, result)
+    result = rpn.calculate("1 2 3 +")
+    self.assertEqual(None, result)
+
+  def test_sub(self):
+    result = rpn.calculate("19 5 -")
+    self.assertEqual(14, result)
+
+  def test_fail(self):
+    result = rpn.calculate("19 -")
+    self.assertEqual(None, result)
+
+  def test_mult(self):
+    result = rpn.calculate("5 2 * 10 *")
+    self.assertEqual(100, result)
+
+  def test_div(self):
+    result = rpn.calculate("5 2 / 2 /")
+    self.assertEqual(1.25, result)
+
+  def test_combine(self):
+    result = rpn.calculate("1 1 + 2 *")
+    self.assertEqual(4, result)
+
